e2b6ab420d14df63404724ecf1195c6a59fc4827
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-08-27 17:13:15 +0200

    [UPD] aggiunto evento keydown per avviare la ricerca con enter

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,3 +1,5 @@
+
+
 <script>
 import { store } from '../store.js';
 
@@ -5,17 +7,29 @@ export default {
     data() {
         return {
             searchQuery: '',
-            store
+            store,
         };
     },
 
     methods: {
         searchContent() {
-            if (this.searchQuery.trim()) {
-                this.store.searchAll(this.searchQuery)
+            if (this.searchQuery.toLowerCase) {
+                store.searchMovie(this.searchQuery)
+                    .then(movieResults => {
+                        return store.searchSeries(this.searchQuery)
+                            .then(seriesResults => {
+                                this.store.results = [...movieResults, ...seriesResults];
+                            });
+                    })
                     .catch(error => {
                         console.error("Errore nella ricerca:", error);
                     });
+            }
+        },
+
+        handleKeyDown(event) {
+            if (event.key === 'Enter') {
+                this.searchContent();
             }
         }
     }
@@ -24,9 +38,24 @@ export default {
 
 <template>
     <div>
-        <input v-model="searchQuery" placeholder="Cerca un film o una serie..." />
+        <input 
+            v-model="searchQuery" 
+            placeholder="Cerca un film o una serie..." 
+            @keydown="handleKeyDown"
+        />
         <button @click="searchContent">Cerca</button>
+
+        <div v-if="store.results.length > 0">
+            <h2>Risultati della ricerca:</h2>
+            <ul>
+                <li v-for="item in store.results" :key="item.id">
+                    {{ item.title || item.name }} ({{ item.vote_average }})
+                    ({{ item.original_title }}) ({{ item.original_language }})
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <style scoped></style>
+
